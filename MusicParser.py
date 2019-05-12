73e8b4336ecb4e6dbcb7459213f3b9ec924e9bec
icowen <34110641+icowen@users.noreply.github.com>
2019-05-12 16:13:18 -0500

    Parsed new midi file.

--- MusicParser.py
+++ MusicParser.py
@@ -1,10 +1,14 @@
 from mido import MidiFile
 
 
+# File used to look at midi files
 def main():
     notes = []
-    mid = MidiFile('bwv10354.mid')
+    mid = MidiFile('MusicFiles/flute pan1.mid')
     for msg in mid:
+        if not msg.is_meta:
+            if msg.type == "note_on":
+                notes.append(msg.note)
         print(msg)
     print(notes)
 
