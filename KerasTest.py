f898ba6faad4c5987884eca639bce4d7233759a0
icowen <ian_cowen@yahoo.com>
2019-08-19 10:54:14 -0500

    Added test sbatch bash script.

--- KerasTest.py
+++ KerasTest.py
@@ -3,21 +3,19 @@ import numpy as np
 from WordConverter import WordConverter
 
 
-def set_up_converter():
+def get_training_data():
     word_file = open('words.txt', 'r')
     words = []
     for w in word_file.readlines():
         words.append(w)
     word_file.close()
-    return WordConverter(words)
+    converter = WordConverter(words)
+    x_list, y_list = converter.get_converted_words()
+    y_list = converter.convert_index_to_letter(y_list)
+    return np.asarray(x_list), np.asarray(y_list)
 
 
-converter = set_up_converter()
-x_list, y_list = converter.get_converted_words()
-y_list = converter.convert_index_to_letter(y_list)
-x_train = np.asarray(x_list)
-y_train = np.asarray(y_list)
-
+x_train, y_train = get_training_data()
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 
 model = tf.keras.models.Sequential()
@@ -25,8 +23,11 @@ model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(50, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(27, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, np.asarray(y_train), epochs=10000)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, np.asarray(y_train), epochs=1000)
 
 #
 # val_loss, val_acc = model.evaluate(x_train, y_train)
@@ -36,5 +37,3 @@ model.fit(x_train, np.asarray(y_train), epochs=10000)
 #
 # for i in range(len(predictions)):
 #     print(np.argmax(predictions[i]))
-
-
---
+++ generated-music.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sbatch \
+--nodes=1 \
+--ntasks-per-node=10 \
+--mem=16000 \
+--time=15:00 \
+--job-name=generatedMusic \
+--output=%j.output.log
+
+cd /home/icowen/GeneratedMusic
+
+python KerasTest.py
