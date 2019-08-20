d46b29af4ad7e7ce4381b0ab10144e39ebf485f2
icowen <ian_cowen@yahoo.com>
2019-08-20 13:41:35 -0500

    update script to use more power

--- generated-music.sh
+++ generated-music.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+cd /home/icowen/GeneratedMusic
+
 sbatch \
 --nodes=1 \
---ntasks-per-node=10 \
---mem=16000 \
---time=15:00 \
+--ntasks-per-node=20 \
+--mem=32000 \
+--time=01:00:00 \
 --job-name=generatedMusic \
 --output=output.log
-
-cd /home/icowen/GeneratedMusic
 
 pip3 install tensorflow --user
 pip3 install keras --user
